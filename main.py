58ab11237cdf904d75287b1046c530aad17990f2
skinnersboxy <joschnei@terpmail.umd.edu>
2019-02-03 18:04:37 -0500

    Dumping current state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import itertools as it
 import pickle
-from math import sqrt
+from math import sqrt, floor
 
 import numpy as np
 
@@ -13,7 +13,7 @@ base_mag_S = 5
 base_mag_A = 5
 base_l = 4
 base_sigma = 0
-
+base_reward = 0
 
 # Trajectory Params
 T = 20
@@ -28,26 +28,20 @@ naive_returns = list()
 sc_returns = list()
 true_returns = list()
 
-for iteration in range(100):
+for iteration in range(1000):
     l = base_l
     sigma = base_sigma
     mag_A = base_mag_A
     mag_S = base_mag_S
-    print(iteration)
+    reward = base_reward + floor(iteration / 10)
+    print(reward)
 
     # Deterministic initial state distribution
     P0 = np.zeros((mag_S))
     P0[0] = 1.0
 
-    P = FLMDP.random_P(mag_S=mag_S,
-                       mag_A=mag_A,
-                       l=l,
-                       mean_reward=100)
-    lmdp = FLMDP(mag_S=mag_S,
-                 mag_A=mag_A,
-                 P=P,
-                 P0=P0,
-                 l=l)
+    P = FLMDP.random_P(mag_S=mag_S, mag_A=mag_A, l=l, mean_reward=reward)
+    lmdp = FLMDP(mag_S=mag_S, mag_A=mag_A, P=P, P0=P0, l=l)
 
     # pi_b = FLMDP.scips_approximable_pi(lmdp=lmdp,
     #                                    Gamma=Gamma,
@@ -55,60 +49,53 @@ for iteration in range(100):
     pi_b = FLMDP.random_pi(lmdp=lmdp)
     pi_e = FLMDP.random_pi(lmdp=lmdp)
 
-    s_b, a_b, r_b = lmdp.simulate(pi=pi_b,
-                                  T=T,
-                                  m=m)
-    s_e, a_e, r_e = lmdp.simulate(pi=pi_e,
-                                  T=T,
-                                  m=m)
+    s_b, a_b, r_b = lmdp.simulate(pi=pi_b, T=T, m=m)
+    s_e, a_e, r_e = lmdp.simulate(pi=pi_e, T=T, m=m)
 
     # Naive Monte-Carlo Policy Estimator
-    hat_b = naive_approx(states=s_b,
-                         actions=a_b,
-                         rewards=r_b,
-                         l=l)
+    hat_b = naive_approx(states=s_b, actions=a_b, rewards=r_b, l=l)
 
     # Sparsity Corrected Policy Estimator
-    tilde_b = sparsity_corrected_approx(states=s_b,
-                                        actions=a_b,
-                                        rewards=r_b,
-                                        Gamma=Gamma,
-                                        lmdp=lmdp)
+    tilde_b = sparsity_corrected_approx(
+        states=s_b, actions=a_b, rewards=r_b, Gamma=Gamma, lmdp=lmdp)
 
-    rho_pi = step_is(pi_b=pi_b,
-                     pi_e=pi_e,
-                     state_samples=s_b,
-                     action_samples=a_b,
-                     reward_samples=r_b,
-                     l=l,
-                     gamma=gamma)
-    rho_hat = step_is(pi_b=hat_b,
-                      pi_e=pi_e,
-                      state_samples=s_b,
-                      action_samples=a_b,
-                      reward_samples=r_b,
-                      l=l,
-                      gamma=gamma)
-    rho_tilde = step_is(pi_b=tilde_b,
-                        pi_e=pi_e,
-                        state_samples=s_b,
-                        action_samples=a_b,
-                        reward_samples=r_b,
-                        l=l,
-                        gamma=gamma)
+    rho_pi = step_is(
+        pi_b=pi_b,
+        pi_e=pi_e,
+        state_samples=s_b,
+        action_samples=a_b,
+        reward_samples=r_b,
+        l=l,
+        gamma=gamma)
+    rho_hat = step_is(
+        pi_b=hat_b,
+        pi_e=pi_e,
+        state_samples=s_b,
+        action_samples=a_b,
+        reward_samples=r_b,
+        l=l,
+        gamma=gamma)
+    rho_tilde = step_is(
+        pi_b=tilde_b,
+        pi_e=pi_e,
+        state_samples=s_b,
+        action_samples=a_b,
+        reward_samples=r_b,
+        l=l,
+        gamma=gamma)
 
     # All the data is there, now to turn it into statistics
 
     # First we compute the RMSE of the two approximations
-    history_sizes = it.repeat(list(range(mag_S+1)), l)
+    history_sizes = it.repeat(list(range(mag_S + 1)), l)
     dist_sizes = it.chain(history_sizes, [list(range(mag_A))])
     history_actions = it.product(*dist_sizes)
 
     rmse_naive = 0.0
     rmse_sc = 0.0
     for history_action in history_actions:
-        rmse_naive += (pi_b[history_action] -
-                       hat_b.get((history_action), 0))**2
+        rmse_naive += (pi_b[history_action] - hat_b.get(
+            (history_action), 0))**2
         rmse_sc += (pi_b[history_action] - tilde_b.get((history_action), 0))**2
     rmses_naive.append(sqrt(rmse_naive))
     rmses_sc.append(sqrt(rmse_sc))
@@ -123,14 +110,14 @@ for iteration in range(100):
     for episode in range(r_e.shape[0]):
         r = 0.0
         for time in range(r_e.shape[1]):
-            r += (gamma ** time) + r_e[episode, time]
+            r += (gamma**time) + r_e[episode, time]
         total_returns += r
     average_return = total_returns / r_e.shape[0]
     true_returns.append(average_return)
 
-pickle.dump(rmses_naive, open('rmses_native_random.pickle', 'wb'))
-pickle.dump(rmses_sc, open('rmses_sc_random.pickle', 'wb'))
-pickle.dump(perfect_returns, open('perfect_returns_random.pickle', 'wb'))
-pickle.dump(naive_returns, open('naive_returns_random.pickle', 'wb'))
-pickle.dump(sc_returns, open('sc_returns_random.pickle', 'wb'))
-pickle.dump(true_returns, open('true_returns_random.pickle', 'wb'))
+pickle.dump(rmses_naive, open('rmses_native_reward.pickle', 'wb'))
+pickle.dump(rmses_sc, open('rmses_sc_reward.pickle', 'wb'))
+pickle.dump(perfect_returns, open('perfect_returns_reward.pickle', 'wb'))
+pickle.dump(naive_returns, open('naive_returns_reward.pickle', 'wb'))
+pickle.dump(sc_returns, open('sc_returns_reward.pickle', 'wb'))
+pickle.dump(true_returns, open('true_returns_reward.pickle', 'wb'))
--- policy_approximators.py
+++ policy_approximators.py
@@ -16,9 +16,7 @@ from _types import Action, History, Position, State
 # rewards = pd.read_csv(rewards_path, header=None)
 
 
-def naive_approx(states: np.ndarray,
-                 actions: np.ndarray,
-                 rewards: np.ndarray,
+def naive_approx(states: np.ndarray, actions: np.ndarray, rewards: np.ndarray,
                  l: int) -> dict:
     """Approximates a policy as a Monte-Carlo estimate of the action taken from each history.
 
@@ -32,11 +30,11 @@ def naive_approx(states: np.ndarray,
 
     history_counts: Dict[History, int] = {}
     history_action_counts: Dict[Tuple[History, Action], int] = {}
-    history_deque: Deque[State] = deque((0,) * l, maxlen=l)
+    history_deque: Deque[State] = deque((0, ) * l, maxlen=l)
 
     for i in range(m):
         trajectory = states[i, :]
-        for t, state in it.islice(enumerate(trajectory), 0, T-1):
+        for t, state in it.islice(enumerate(trajectory), 0, T - 1):
             history_deque.appendleft(state)
             history_counts[tuple(history_deque)] = history_counts.get(
                 tuple(history_deque), 0) + 1
@@ -47,18 +45,15 @@ def naive_approx(states: np.ndarray,
     naive_probabilities: Dict[tuple, float] = {}
 
     for (history, action), count in history_action_counts.items():
-        naive_probabilities[tuple(history) + (action,)] = float(
-            count) / float(history_counts[history])
+        naive_probabilities[tuple(history) + (
+            action, )] = float(count) / float(history_counts[history])
 
     return naive_probabilities
 
 
-def sc_probability(history: History,
-                   action: Action,
-                   Gamma: float,
+def sc_probability(history: History, action: Action, Gamma: float,
                    positional_state_counts: dict,
-                   positional_state_action_counts: dict,
-                   l: int) -> float:
+                   positional_state_action_counts: dict, l: int) -> float:
     """Gets the probability estimate of an action given a specific history.
 
     """
@@ -73,11 +68,8 @@ def sc_probability(history: History,
     return probability * (1.0 - Gamma) / (1.0 - Gamma**(l))
 
 
-def sparsity_corrected_approx(states: np.ndarray,
-                              actions: np.ndarray,
-                              rewards: np.ndarray,
-                              Gamma: float,
-                              lmdp) -> dict:
+def sparsity_corrected_approx(states: np.ndarray, actions: np.ndarray,
+                              rewards: np.ndarray, Gamma: float, lmdp) -> dict:
     """ Approximates a policy using the sparsity corrected method.
 
     """
@@ -93,25 +85,26 @@ def sparsity_corrected_approx(states: np.ndarray,
     assert states.shape[1] - 1 == T
 
     positional_state_counts: Dict[Tuple[State, Position], int] = {}
-    positional_state_action_counts: Dict[Tuple[State, Position, Action], int] = {
-    }
-    history_deque: Deque[State] = deque((0,) * l, maxlen=l)
+    positional_state_action_counts: Dict[Tuple[State, Position, Action],
+                                         int] = {}
+    history_deque: Deque[State] = deque((0, ) * l, maxlen=l)
 
     for i in range(m):
         trajectory = states[i, :]
-        for t, state in it.islice(enumerate(trajectory), 0, T-1):
+        for t, state in it.islice(enumerate(trajectory), 0, T - 1):
             history_deque.appendleft(state)
             for k, state_in_history in enumerate(history_deque):
                 state_context: Tuple[State, Position] = (state_in_history, k)
-                positional_state_counts[state_context] = positional_state_counts.get(
-                    state_context, 0) + 1
+                positional_state_counts[
+                    state_context] = positional_state_counts.get(
+                        state_context, 0) + 1
                 state_action_context: Tuple[State, Position, Action] = (
                     state_in_history, k, actions[i, t])
                 positional_state_action_counts[state_action_context] = \
                     positional_state_action_counts.get(
                         state_action_context, 0) + 1
 
-    history_sizes = it.repeat(list(range(mag_S+1)), l)
+    history_sizes = it.repeat(list(range(mag_S + 1)), l)
     history_action_sizes = it.chain(history_sizes, [list(range(mag_A))])
     history_actions = it.product(*history_action_sizes)
 
